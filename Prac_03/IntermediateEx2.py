1063a3f9971ba3b1aec15fe20aeaf9db41fd013f
jarodhine <jarod.hine@my.jcu.edu.au>
2017-09-15 10:26:01 +1000

    Add Splice function

--- Prac_03/IntermediateEx2.py
+++ Prac_03/IntermediateEx2.py
@@ -1,12 +1,17 @@
 def main():
-    name = GetName()
-    print(name)
+    name = ""
+    name = GetName(name)
+    SpliceName(name)
 
 
-def GetName():
+
+def GetName(name):
     name = str(input("Enter name: "))
-    while name == ():
+    while name == "":
         name = str(input("Invalid choice please enter a name: "))
     return name
+    
+def SpliceName(name):
+  print(name[::2])
 
 main()
