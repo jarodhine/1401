cbbf92e574b6fed07cac50ee4f4c4520f3929083
jarodhine <jarod.hine@my.jcu.edu.au>
2017-09-15 10:36:02 +1000

    Add frequency and error checking

--- Prac_03/IntermediateEx2.py
+++ Prac_03/IntermediateEx2.py
@@ -1,17 +1,35 @@
 def main():
+    frequency = 0
+    frequency = GetFrequency(frequency)
     name = ""
     name = GetName(name)
-    SpliceName(name)
+    SpliceName(name,frequency)
 
-
+def GetFrequency(frequency):
+  try:
+    frequency = int(input("Enter frequency: "))
+  except:
+    print("")
+  while frequency <= 0:
+    try:
+      frequency = int(input("Invalid choice please enter frequency: "))
+    except:
+      print("")
+  return frequency
 
 def GetName(name):
+  try:
     name = str(input("Enter name: "))
-    while name == "":
-        name = str(input("Invalid choice please enter a name: "))
-    return name
+  except:
+    print("")
+  while name == "":
+    try:
+      name = str(input("Invalid choice please enter name: "))
+    except:
+      print("")
+  return name
     
-def SpliceName(name):
-  print(name[::2])
+def SpliceName(name,frequency):
+  print(name[::frequency])
 
 main()
