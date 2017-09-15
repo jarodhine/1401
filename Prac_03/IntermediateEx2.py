05ccbc18b2629e260a335b5e302ddd62d26cca79
jarodhine <jarod.hine@my.jcu.edu.au>
2017-09-15 12:20:34 +1000

    Fix Indentation

--- Prac_03/IntermediateEx2.py
+++ Prac_03/IntermediateEx2.py
@@ -6,30 +6,30 @@ def main():
     SpliceName(name,frequency)
 
 def GetFrequency(frequency):
-  try:
-    frequency = int(input("Enter frequency: "))
-  except:
-    print("")
-  while frequency <= 0:
     try:
-      frequency = int(input("Invalid choice please enter frequency: "))
+      frequency = int(input("Enter frequency: "))
     except:
       print("")
-  return frequency
+    while frequency <= 0:
+      try:
+        frequency = int(input("Invalid choice please enter frequency: "))
+      except:
+        print("")
+    return frequency
 
 def GetName(name):
-  try:
-    name = str(input("Enter name: "))
-  except:
-    print("")
-  while name == "":
     try:
-      name = str(input("Invalid choice please enter name: "))
+      name = str(input("Enter name: "))
     except:
       print("")
-  return name
+    while name == "":
+      try:
+        name = str(input("Invalid choice please enter name: "))
+      except:
+        print("")
+    return name
     
 def SpliceName(name,frequency):
-  print(name[::frequency])
+    print(name[::frequency])
 
 main()
