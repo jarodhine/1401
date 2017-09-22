6097509447cbf9ac7b3cbe23b4f34be5066863f5
jarodhine <jarod.hine@my.jcu.edu.au>
2017-09-22 11:58:01 +1000

    Add Prac 6 ScratchEx 1

---
+++ Prac_06/ScratchEx1.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+M_TO_KM = 1.609
+
+
+class Converter(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('ScratchEx1.kv')
+        return self.root
+
+    def calculate(self):
+        value = self.validMiles()
+        result = value * M_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def increment(self, change):
+        value = self.validMiles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.calculate()
+
+    def validMiles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+Converter().run()
