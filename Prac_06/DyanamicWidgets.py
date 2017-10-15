7733533e1d5c3e8e111c89f7bda0b5d7bc02210d
jarodhine <jarod.hine@my.jcu.edu.au>
2017-10-15 23:13:56 +1000

    Add DynamicWidgets

--- Prac_06/DyanamicWidgets.py
+++ Prac_06/DyanamicWidgets.py
@@ -1,4 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.button import Label
 
-NAMES = {"Bob", "Bill", "Wendy", "Jack"}
+NAMES = ["John", "Bill", "Bob"]
+
+class DynamicWidgets(App):
+
+    def build(self):
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('DynamicWidgets.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in NAMES:
+            temp_label = Label(text=name)
+            self.root.ids.entriesBox.add_widget(temp_label)
+
+DynamicWidgets().run()
